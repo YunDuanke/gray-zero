c16e90b1d57f8b5145d2431d487001bbf9ae955f
陈昱衡 <chenyuheng666@126.com>
2024-11-03 17:07:28 +0800

    feat：input

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,16 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import TransMenu from "./index";
+import Input from "./input";
 
 const meta = {
-  title: "TransMenu",
-  component: TransMenu,
+  title: "Input",
+  component: Input,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     // layout: "fullscreen",
   },
-} satisfies Meta<typeof TransMenu>;
+} satisfies Meta<typeof Input>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -22,18 +22,10 @@ export const Default: Story = {
         display: "flex",
         alignItems: "center",
         justifyItems: "center",
-        minHeight: 300,
+        width: 300,
       }}
     >
-      <TransMenu defaultIndex="0" {...args}>
-        <TransMenu.Item>cool link</TransMenu.Item>
-        <TransMenu.Item>cool link 2</TransMenu.Item>
-        <TransMenu.Item disabled>disabled</TransMenu.Item>
-        <TransMenu.SubMenu title="下拉选项">
-          <TransMenu.Item>下拉选项一</TransMenu.Item>
-          <TransMenu.Item>下拉选项二</TransMenu.Item>
-        </TransMenu.SubMenu>
-      </TransMenu>
+      <Input {...args} />
     </div>
   ),
 };

d0bee53404521addc74ce3a2620a71010a5609d5
陈昱衡 <chenyuheng666@126.com>
2024-11-21 21:22:51 +0800

    feat: add travis

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,14 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|md)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/blocks"
   ],
   docs: {
     //👇 See the table below for the list of supported options
